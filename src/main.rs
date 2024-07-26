b31c904375e3b04fdc898a3b6c2df0922a62bf64
Benjamin Tamasi <halftome@gmail.com>
2024-07-26 14:58:58 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use std::error::Error;
 use ant::drivers::*;
-use ant::messages::{RxMessage, AntMessage};
-use ant::messages::config::{ChannelId, ChannelRfFrequency, ChannelType, DeviceType, EnableExtRxMessages, SetNetworkKey, AssignChannel, TransmissionType};
-use ant::messages::control::{ResetSystem, OpenRxScanMode};
-use rusb::{Device, DeviceList};
+use ant::messages::config::{
+    AssignChannel, ChannelId, ChannelRfFrequency, ChannelType, DeviceType, EnableExtRxMessages,
+    SetNetworkKey, TransmissionType,
+};
+use ant::messages::control::{OpenRxScanMode, ResetSystem};
+use ant::messages::{AntMessage, RxMessage};
+use clap::Parser;
 use packed_struct::PackedStruct;
+use rusb::{Device, DeviceList};
+use std::error::Error;
+use std::io::Write;
+use std::net::TcpStream;
 
 const NETWORK_KEY: [u8; 8] = [0xB9, 0xA5, 0x21, 0xFB, 0xBD, 0x72, 0xC3, 0x45];
 const RF_FREQ: u8 = 57;
 
+/// Dump ANT+ data from the air
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Optional TCP server to stream raw ANT+ data to
+    #[arg(short, long)]
+    server: Option<String>,
+}
+
 fn main() -> std::io::Result<()> {
-    //let mut stream = TcpStream::connect("127.0.0.1:9999").unwrap();
+    let args = Args::parse();
+    let mut stream = match args.server {
+        Some(url) => Some(TcpStream::connect(url)?),
+        None => None,
+    };
 
     let devices: Vec<Device<_>> = DeviceList::new()
         .expect("Unable to lookup usb devices")
@@ -23,17 +42,36 @@ fn main() -> std::io::Result<()> {
 
     // open RX Scan mode
     driver.send_message(&ResetSystem::new()).unwrap();
-    driver.send_message(&SetNetworkKey::new(0, NETWORK_KEY)).unwrap();
-    driver.send_message(&AssignChannel::new(0, ChannelType::SharedReceiveOnly, 0, None)).unwrap();
-    driver.send_message(&ChannelId::new(
-        0,
-        0,
-        DeviceType::new(0.into(), false),
-        TransmissionType::new_wildcard(),
-    )).unwrap();
-    driver.send_message(&ChannelRfFrequency::new(0, RF_FREQ)).unwrap();
-    driver.send_message(&EnableExtRxMessages::new(true)).unwrap();
-    driver.send_message(&OpenRxScanMode { synchronous_channel_packets_only: None }).unwrap();
+    driver
+        .send_message(&SetNetworkKey::new(0, NETWORK_KEY))
+        .unwrap();
+    driver
+        .send_message(&AssignChannel::new(
+            0,
+            ChannelType::SharedReceiveOnly,
+            0,
+            None,
+        ))
+        .unwrap();
+    driver
+        .send_message(&ChannelId::new(
+            0,
+            0,
+            DeviceType::new(0.into(), false),
+            TransmissionType::new_wildcard(),
+        ))
+        .unwrap();
+    driver
+        .send_message(&ChannelRfFrequency::new(0, RF_FREQ))
+        .unwrap();
+    driver
+        .send_message(&EnableExtRxMessages::new(true))
+        .unwrap();
+    driver
+        .send_message(&OpenRxScanMode {
+            synchronous_channel_packets_only: None,
+        })
+        .unwrap();
     loop {
         match driver.get_message() {
             Ok(None) => (),
@@ -42,6 +80,9 @@ fn main() -> std::io::Result<()> {
                     let mut raw = vec![];
                     raw_data(&msg, &mut raw).unwrap();
                     println!("{:02X?}", raw);
+                    if let Some(stream) = &mut stream {
+                        stream.write_all(raw.as_slice())?
+                    }
                 }
                 //_msg => println!("Got: {:#?}", _msg),
                 _msg => (),
@@ -58,10 +99,15 @@ fn raw_data(msg: &AntMessage, out: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
             out.extend(msg2.payload.pack()?);
             let ext_info = msg2.extended_info.ok_or("missing extended info")?;
             out.extend(ext_info.flag_byte.pack()?);
-            out.extend(ext_info.channel_id_output.ok_or("missing channel id")?.pack()?);
+            out.extend(
+                ext_info
+                    .channel_id_output
+                    .ok_or("missing channel id")?
+                    .pack()?,
+            );
             out.push(msg.checksum);
             Ok(())
         }
-        _ => Err("Only broadcast data is supported for now".into())
+        _ => Err("Only broadcast data is supported for now".into()),
     }
 }
